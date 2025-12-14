ebd1ffb18e40e40e8ac51ff02fad7ac51d301b28
Chidumam Amadi-Obi <amadiobichidumam@gmail.com>
2025-12-14 22:23:51 +0000

    testing integration of al cpu components & opcodes

--- src/cpu.h
+++ src/cpu.h
@@ -6,19 +6,31 @@
 #include "memory.h"
 #include "control_unit.h"
 
-static DecodedInst inst;
-
-void initCPU(CPU *cpu){
+void initCPU(CPU *cpu){ 
     initMemory(cpu); 
     initCtrlUnit(cpu);
     loadProgram(cpu);
     cpu->isRunning = true;
+    cpu->errorFlag = false;    
 }
 
-void runCPU(CPU *cpu){
-    cpuFetch(cpu);
+void runCPU(CPU *cpu){ // fetch decode execute
+    static DecodedInst inst;
+
+    cpuFetch(cpu,PRINT_PC);
     inst = cpuDecode(cpu);
     cpuExecute(&inst,cpu);        
+}
+
+void debugCPU(CPU *cpu){
+    debugRAM(PRINT_RAM_MODE,cpu);
+
+    printf("Zero: %d ",cpu->zeroFlag); 
+    printf("\nAbv:  %d ",(!cpu->negativeFlag&&!cpu->zeroFlag)); 
+    printf("\nNeg:  %d ",cpu->negativeFlag); 
+    printf("\nOvf:  %d ",cpu->overflowFlag); 
+    printf("\nError:  %d ",cpu->errorFlag); 
+    printf("\nReg 1:  %d ",cpu->gpRegs[1]); 
 }
 
 
--- src/main.c
+++ src/main.c
@@ -1,14 +1,9 @@
 #include "control_unit.h"
 #include "cpu.h"
 
-int main(void) { 
-    initCPU(&cpu); // debugRAM(PRINT_PROGRAM_MODE,&cpu);
-
-    static DecodedInst instructions; 
-    while (cpu.isRunning){
-        printf("%d \t",cpu.programCounter);
-        runCPU(&cpu);
-    }
-    return 0;
+int main(void) { // testing ld str and jmp ops
+    CPU cpu;
+    initCPU(&cpu);
+    while (cpu.isRunning) runCPU(&cpu);
+    debugCPU(&cpu);
 }
-
