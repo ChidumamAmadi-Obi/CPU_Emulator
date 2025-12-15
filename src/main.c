5c0899d6f3bdcd0b754f8552ffbe97849d9e3a16
Chidumam Amadi-Obi <amadiobichidumam@gmail.com>
2025-12-15 08:12:15 +0000

    polish and add more comments

--- src/main.c
+++ src/main.c
@@ -1,9 +1,13 @@
 #include "control_unit.h"
 #include "cpu.h"
 
-int main(void) { // testing ld str and jmp ops
+int main(void) { 
     CPU cpu;
     initCPU(&cpu);
-    while (cpu.isRunning) runCPU(&cpu);
-    debugCPU(&cpu);
+
+    while (cpu.isRunning) {
+        runCPU(&cpu);
+    } debugCPU(&cpu);
+
+    return 0;
 }
