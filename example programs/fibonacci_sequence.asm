1e238220287c05a9a0d13a22faa498c5fa8bf771
Chidumam Amadi-Obi <amadiobichidumam@gmail.com>
2026-01-15 16:18:57 +0000

    Update fibonacci_sequence.asm

--- example programs/fibonacci_sequence.asm
+++ example programs/fibonacci_sequence.asm
@@ -1,28 +1,18 @@
-;SUMS ARRAY OF NUMNERS (sum = 76)
-;36 CYCLES
+;COMPUTES fib(8) = 21
+;IN 47 CYCLES
+LI r0 0;
+LI r1 1; 
+LI r2 7; loop count (0-7 is 8 loops)
+LI r3 0; result
 
-; initialize array
-LI r15 12;
-STR 0xF0 r15;
-STR 0xF1 r15;
-LI r15 48;
-STR 0xF2 r15;
-LI r15 4;
-STR 0xF3 r15;
-LI r15 0; clear r0
+LOOP: 
+    ADD r3 r0 r1;
+    MOV r0 r1;
+    MOV r1 r3;
+    SUBI r2 r2 1; decriment counter
+    JMP_ZRO END; if result is zero, jump to FINISH
+    JMP LOOP; jump to LOOP
 
-LI r0 0; sum
-LI r1 4; no of elements in array 
-LI r2 0xF0; pointer to array start
-
-START:
-    LD r3 *r2; load array element with pointer
-    ADD r0 r0 r3; add to sum
-    ADDI r2 r2 1; increment pointer
-    SUBI r1 r1 1; decrement counter
-    JMP_ZRO FINISH; when the end of the array is reached, jump to FINISH
-    JMP START; jump to START
-
-FINISH:
-    STR 0xFE r0; store result in RAM location 0xFE
+END: 
+    STR 0xFE r3; store result in RAM location 0xFE
     HALT; end program
