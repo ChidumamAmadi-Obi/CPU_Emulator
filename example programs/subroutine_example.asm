f8fe7f47f2fd626a5de8d0a048b51c58789ebc04
Chidumam Amadi-Obi <amadiobichidumam@gmail.com>
2026-01-16 15:08:19 +0000

    added subroutine example with sum of array

---
+++ example programs/subroutine_example.asm
@@ -0,0 +1,36 @@
+;SUMS ARRAY OF NUMBERS WITH SOUROUTINES (sum = 76)
+;EXECUTES IN 46 CYCLES
+
+; MAIN PROGRAM ______________________________________________________________________________
+CALL INIT_ARRAY ; calls array subroutine
+LI r0 0; sum
+LI r1 4; no of elements in array 
+LI r2 0xF0; pointer to array start
+
+START:
+    LD r3 *r2; load array element with pointer
+    ADD r0 r0 r3; add to sum
+    CALL HANDLE_PTR_CNT
+    JMP_ZRO FINISH; when the end of the array is reached, jump to FINISH
+    JMP START; jump to START
+
+FINISH:
+    STR 0xFE r0; store result in RAM location 0xFE
+    HALT; end program
+
+; SUBROUTINES ______________________________________________________________________________
+INIT_ARRAY:    ; initialize array
+    LI r15 12; temp
+    STR 0xF0 r15;
+    STR 0xF1 r15;
+    LI r15 48;
+    STR 0xF2 r15;
+    LI r15 4;
+    STR 0xF3 r15;
+    LI r15 0; clear r0
+    RET
+
+HANDLE_PTR_CNT: ; handles ptr inc and counter decs
+    ADDI r2 r2 1; increment pointer
+    SUBI r1 r1 1; decrement counter
+    RET
